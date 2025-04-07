781d22eb6d38350c8a2795cd7d3f87e6b27959d7
Lucas Henrique de Sousa <163554117+LucasHenriqueSousa@users.noreply.github.com>
2025-04-07 10:33:24 -0300

    Projeto de agenda ultilizando banco de dados.

--- minhaagenda/app/src/main/java/com/test/minhaagenda/model/Usuarios.kt
+++ minhaagenda/app/src/main/java/com/test/minhaagenda/model/Usuarios.kt
@@ -1,8 +1,14 @@
 package com.test.minhaagenda.model
 
 import androidx.room.Entity
-
+import androidx.room.ColumnInfo
 @Entity(tableName = "table-usuarios")
 
-class Usuarios {
-}
+data class Usuarios (
+    @ColumnInfo(name = "nome")
+    val nome: String,
+    @ColumnInfo(name = "email")
+    val email: String,
+    @ColumnInfo(name = "senha")
+    val senha: String
+)
